342490c121da8ecf2bfbaebf7715ab7d1a73fa25
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2022-10-07 13:06:14 +0900

    Exception Filters 終了

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
-import { CreateCatDto } from './dto/create-cat.dto';
-import { CatsService } from './cats.service';
-import { Cat } from './interfaces/cat.interface';
+import {Controller, Get, Post, Body, HttpException, HttpStatus, UseFilters} from '@nestjs/common';
+import {CreateCatDto} from './dto/create-cat.dto';
+import {CatsService} from './cats.service';
+import {Cat} from './interfaces/cat.interface';
+import {HttpExeptionFilter} from 'src/middleware/http-exception.filter';
 
 @Controller('cats')
+@UseFilters(new HttpExeptionFilter())
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
@@ -14,6 +16,12 @@ export class CatsController {
 
   @Get()
   async findAll(): Promise<Cat[]> {
+    throw new HttpException({
+      status: HttpStatus.FORBIDDEN,
+      error: 'This is a custom message',
+    }, HttpStatus.FORBIDDEN);
+
     return this.catsService.findAll();
   }
 }
+
